5a3af1d7246ece29fd36ce9f50b030c7c357b402
jurufola <moulaye.haidara@laposte.net>
2021-05-05 12:12:05 +0200

    Mise en place setters et getters

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -15,4 +15,32 @@ public class Personne {
         this.prenom = prenom;
         this.adresse = adresse;
     }
+
+    public void affichNom(){
+        System.out.println("Je m'appelle : " + this.prenom.toUpperCase() + " " + this.nom.toUpperCase());
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setAdresse(AdressePostale adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public AdressePostale getAdresse() {
+        return adresse;
+    }
 }
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -7,6 +7,9 @@ public class TestPersonne {
     public static void main(String[] args) {
         AdressePostale adresse = new AdressePostale("870", "rue de la valsière",  "34790", "Grabels");
         Personne moulaye = new Personne("HAÏDARA", "Moulaye", adresse);
+        Personne awa = new Personne("HAÏDARA","Awa");
+        awa.setAdresse(adresse);
+        awa.affichNom();
         Personne aman = new Personne("HAÏDARA", "Aman", adresse);
         System.out.println("Moulaye a pour nom : " + moulaye.nom);
         System.out.println("Aman a pour libellé de rue : " +  aman.adresse.libelleRue);
