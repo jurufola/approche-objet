2160a436b069c85fa0ad24b5cd0b0008b212122c
jurufola <moulaye.haidara@laposte.net>
2021-05-11 11:19:53 +0200

    Exercice 1

---
+++ src/listes/CreationListe.java
@@ -0,0 +1,18 @@
+package listes;
+
+import java.util.ArrayList;
+
+public class CreationListe {
+    public static void main(String[] args) {
+        ArrayList<Integer> listEntier = new ArrayList<Integer>();
+        for (int i = 0; i < 100; i++) {
+            listEntier.add(i+1);
+        }
+        System.out.println("La taille de mon ArrayList d'entiers est : " + listEntier.size());
+
+        //Affichage
+        for (Integer integer : listEntier) {
+            System.out.println(integer);
+        }
+    }
+}
